c5b44f196dbc4222b71570f683fa03307120c68b
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2022-12-31 20:30:34 +0800

    feat: update 785

--- src/785.判断二分图.js
+++ src/785.判断二分图.js
@@ -9,41 +9,77 @@
  * @param {number[][]} graph
  * @return {boolean}
  */
+// solution1: dfs
+// var isBipartite2 = function(graph) {
+//   let isOK = true
+//   const n = graph.length
+//   const colorArrofBoolean = new Array(n).fill(false)
+//   const vistedArrOfBoolean = new Array(n).fill(false)
+//   // 图不一定是联通的
+//   for (let i = 0; i < n; i++) {
+//     if (!vistedArrOfBoolean[i]) {
+//       traverse(graph, i)
+//     }
+//   }
+//   return isOK
+
+//   function traverse(graph, v) {
+//     if (!isOK) {
+//       return
+//     }
+//     vistedArrOfBoolean[v] = true
+//     for (let j = 0; j < graph[v].length; j++) {
+//       const w = graph[v][j]
+//       if (!vistedArrOfBoolean[w]) {
+//         colorArrofBoolean[w] = !colorArrofBoolean[v]
+//         traverse(graph, w)
+//       } else {
+//         if (colorArrofBoolean[w] === colorArrofBoolean[v]) {
+//           isOK = false
+//         }
+//       }
+//     }
+//   }
+// }
+
+// solution2: bfs
 var isBipartite = function(graph) {
-  let isOK = true
+  let isOk = true
   const n = graph.length
-  const colorArrofBoolean = new Array(n).fill(false)
-  const vistedArrOfBoolean = new Array(n).fill(false)
-  // 图不一定是联通的
+  const colorArrOfBoolean = new Array(n).fill(false)
+  const visitedArrOfBoolean = new Array(n).fill(false)
   for (let i = 0; i < n; i++) {
-    if (!vistedArrOfBoolean[i]) {
-      traverse(graph, i)
+    if (!visitedArrOfBoolean[i]) {
+      bfs(graph, i)
     }
   }
-  return isOK
+  return isOk
 
-  function traverse(graph, v) {
-    if (!isOK) {
-      return
-    }
-    vistedArrOfBoolean[v] = true
-    for (let j = 0; j < graph[v].length; j++) {
-      const w = graph[v][j]
-      if (!vistedArrOfBoolean[w]) {
-        colorArrofBoolean[w] = !colorArrofBoolean[v]
-        traverse(graph, w)
-      } else {
-        if (colorArrofBoolean[w] === colorArrofBoolean[v]) {
-          isOK = false
+  function bfs(graph, start) {
+    const queue = [start]
+    visitedArrOfBoolean[start] = true
+    while (queue.length && isOk) {
+      const v = queue.shift()
+      for (let i = 0; i < graph[v].length; i++) {
+        const w = graph[v][i]
+        if (!visitedArrOfBoolean[w]) {
+          colorArrOfBoolean[w] = !colorArrOfBoolean[v]
+          visitedArrOfBoolean[w] = true
+          queue.push(w)
+        } else {
+          if (colorArrOfBoolean[v] === colorArrOfBoolean[w]) {
+            isOk = false
+          }
         }
       }
     }
   }
 }
+
 // isBipartite([
-//   [1, 2, 3],
+//   [1, 3],
 //   [0, 2],
-//   [0, 1, 3],
+//   [1, 3],
 //   [0, 2]
 // ])
 // @lc code=end
