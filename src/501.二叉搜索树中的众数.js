d666b6260819922fe54970f4b416fe626e9939dc
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2023-01-01 15:50:07 +0800

    feat: add 501

--- src/501.二叉搜索树中的众数.js
+++ src/501.二叉搜索树中的众数.js
@@ -4,6 +4,8 @@
  * [501] 二叉搜索树中的众数
  */
 
+const { traverse } = require('@babel/core')
+
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -18,6 +20,32 @@
  * @return {number[]}
  */
 // !! no extra hash table
-var findMode = function(root) {}
+var findMode = function(root) {
+  let res = []
+  let maxCount = 0
+  let curCount = 0
+  let preVal = null
+  traverse(root)
+  return res
+
+  function traverse(root) {
+    if (!root) return
+    traverse(root.left)
+    if (root.val === preVal) {
+      curCount++
+    } else {
+      curCount = 1
+    }
+
+    if (curCount === maxCount) {
+      res.push(root.val)
+    } else if (curCount > maxCount) {
+      res = [root.val]
+      maxCount = curCount
+    }
+    preVal = root.val
+    traverse(root.right)
+  }
+}
 
 // @lc code=end
