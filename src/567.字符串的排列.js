c999457795e59798da0450461b098f742ae17563
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2023-02-27 09:18:25 +0800

    chore: update 567

--- src/567.字符串的排列.js
+++ src/567.字符串的排列.js
@@ -11,21 +11,18 @@
  * @return {boolean}
  */
 var checkInclusion = function(target, source) {
-  // since we need to count each char, we init all container with 'Map' instead of set
   const needObj = {}
   const windowObj = {}
-  // init two window
   for (let i = 0; i < target.length; i++) {
     let char = target[i]
-    if (!needObj[char]) {
-      needObj[char] = 1
-    } else {
-      needObj[char]++
-    }
-    if (!windowObj[char]) {
+    // init windowObj and needObj
+    if (needObj[char] === undefined) {
+      needObj[char] = 0
       windowObj[char] = 0
     }
+    needObj[char]++
   }
+
   let left = 0,
     right = 0,
     // valid charater length, will be used to compare with need's length
@@ -33,15 +30,14 @@ var checkInclusion = function(target, source) {
 
   // expand window: move right pointer
   while (right < source.length) {
-    const char = source[right]
+    const c = source[right]
     right++
 
     // update data: window, validNum
-    if (needObj[char]) {
-      windowObj[char]++
-
+    if (needObj[c]) {
+      windowObj[c]++
       // window: {a: 2, b: 1} => need: {a:2} => a is valid,  valid++
-      if (windowObj[char] == needObj[char]) {
+      if (windowObj[c] == needObj[c]) {
         validNum++
       }
     }
@@ -49,7 +45,9 @@ var checkInclusion = function(target, source) {
     // shrink window condition
     while (right - left > target.length) {
       // check terminal condition
-      if (validNum == Object.keys(needObj).length) return true
+      if (validNum == Object.keys(needObj).length) {
+        return true
+      }
       // move left pointer,  reverse update
       const d = source[left]
 
@@ -64,8 +62,6 @@ var checkInclusion = function(target, source) {
   }
   return false
 }
-
-checkInclusion('ab', 'eidboaoo')
 // @lc code=end
 
 module.exports = { checkInclusion }
